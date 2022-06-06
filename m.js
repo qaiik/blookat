d4880f2aaf297fdc2fc4237468adb92cb27787ef
qaiik <82181231+qaiik@users.noreply.github.com>
2022-06-06 13:48:32 -0600

    Update m.js

--- m.js
+++ m.js
@@ -1,1 +1,15 @@
-let blgid = Math.floor(100000 + Math.random() * 900000);
+function endpoint(id) {
+  return `https://fb.blooket.com/c/firebase/id?id=${id}`
+}
+
+let checked = 0;
+
+let s = Date.now();
+setInterval(()=>{
+  let gid = Math.floor(100000 + Math.random() * 900000)
+  checked+=1
+  fetch(endpoint(String(gid))).then(res=>res.json()).then(data=>{
+    checked%15===0 ? console.log(checked) : ""
+    data.success === true ? console.log(gid, data.success) : ""
+  })
+}, 50)
